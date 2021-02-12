89bae7bbee846d4a1801880872b36484418fac3c
Enrique <sotomaque@gmail.com>
2021-02-12 11:10:53 -0800

    first commit

---
+++ constants/images.js
@@ -0,0 +1,13 @@
+const banner = require('../assets/images/banner.png');
+const focus = require('../assets/images/focus.png');
+const promoBanner = require('../assets/images/promo-banner.png');
+const usFlag = require('../assets/images/us-flag.jpg');
+const wallieLogo = require('../assets/images/wallie-logo.png');
+
+export default {
+  banner,
+  focus,
+  promoBanner,
+  usFlag,
+  wallieLogo,
+};
