54221869795cdcabb4ed83e127d999258b2c0427
Enrique <sotomaque@gmail.com>
2021-02-13 02:07:25 -0800

    worked on signin page

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {
   View,
@@ -19,24 +20,201 @@ import {COLORS, SIZES, icons, images, FONTS} from '../constants';
 const SignUp = () => {
   function renderHeader() {
     return (
-      <TouchableOpacity
+      <View
         style={{
           flexDirection: 'row',
           alignItems: 'center',
           marginTop: SIZES.padding * 6,
           paddingHorizontal: SIZES.padding * 2,
-        }}
-        onPress={() => console.log('Sign up pressed')}>
-        <Image
-          source={icons.back}
-          resizeMode="contain"
+        }}>
+        <TouchableOpacity onPress={() => console.log('Sign up pressed')}>
+          <Image
+            source={icons.back}
+            resizeMode="contain"
+            style={{
+              width: 20,
+              height: 20,
+              tintColor: COLORS.white,
+            }}
+          />
+        </TouchableOpacity>
+        <Text
           style={{
-            width: 20,
-            height: 20,
-            tintColor: COLORS.white,
-          }}
+            marginLeft: SIZES.padding * 1.5,
+            color: COLORS.white,
+            ...FONTS.h4,
+          }}>
+          Sign Up
+        </Text>
+      </View>
+    );
+  }
+
+  function renderLogo() {
+    return (
+      <View
+        style={{
+          marginTop: SIZES.padding * 5,
+          height: 100,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <Image
+          source={images.wallieLogo}
+          resizeMode="contain"
+          style={{width: '60%'}}
         />
-      </TouchableOpacity>
+      </View>
+    );
+  }
+
+  function renderForm() {
+    return (
+      <View
+        style={{
+          marginTop: SIZES.padding * 3,
+          marginHorizontal: SIZES.padding * 3,
+        }}>
+        {/* Full Name */}
+        <View style={{marginTop: SIZES.padding * 3}}>
+          <Text
+            style={{
+              color: COLORS.lightGreen,
+              ...FONTS.body3,
+            }}>
+            Full Name
+          </Text>
+          <TextInput
+            style={{
+              marginVertical: SIZES.padding,
+              borderBottomColor: COLORS.white,
+              borderBottomWidth: 1,
+              height: 40,
+              color: COLORS.white,
+              ...FONTS.body3,
+            }}
+            placeholder="Enter Full Name"
+            placeholderTextColor={COLORS.white}
+            selectionColor={COLORS.white}
+          />
+        </View>
+
+        {/* Phone Number */}
+        <View
+          style={{
+            marginTop: SIZES.padding * 2,
+          }}>
+          <Text style={{color: COLORS.lightGreen, ...FONTS.body3}}>
+            Phone Number
+          </Text>
+          <View style={{flexDirection: 'row'}}>
+            {/* Country Code */}
+            <TouchableOpacity
+              style={{
+                width: 100,
+                height: 50,
+                marginHorizontal: 5,
+                borderBottomColor: COLORS.white,
+                borderBottomWidth: 1,
+                flexDirection: 'row',
+                ...FONTS.body2,
+              }}
+              onPress={() => {}}>
+              {/* Drop Down */}
+              <View style={{justifyContent: 'center'}}>
+                <Image
+                  source={icons.down}
+                  style={{width: 10, height: 10, tintColor: COLORS.white}}
+                />
+              </View>
+              {/* Country Flag */}
+              <View style={{justifyContent: 'center', marginLeft: 5}}>
+                <Image
+                  source={images.usFlag}
+                  resizeMode="contain"
+                  style={{width: 30, height: 30}}
+                />
+              </View>
+              {/* Country Code */}
+              <View style={{justifyContent: 'center', marginLeft: 5}}>
+                <Text
+                  style={{color: COLORS.white, paddingLeft: 5, ...FONTS.body3}}>
+                  +1
+                </Text>
+              </View>
+            </TouchableOpacity>
+            {/* Phone Number Input */}
+            <TextInput
+              style={{
+                flex: 1,
+                marginVertical: SIZES.padding,
+                borderBottomColor: COLORS.white,
+                borderBottomWidth: 1,
+                height: 40,
+                color: COLORS.white,
+                ...FONTS.body3,
+              }}
+              placeholderTextColor={COLORS.white}
+              selectionColor={COLORS.white}
+              placeholder="Enter Phone Number"
+            />
+          </View>
+        </View>
+
+        {/* Password */}
+        <View style={{marginTop: SIZES.padding * 2}}>
+          <Text style={{color: COLORS.lightGreen, ...FONTS.body3}}>
+            Password
+          </Text>
+          <TextInput
+            style={{
+              marginVertical: SIZES.padding,
+              borderBottomWidth: 1,
+              borderBottomColor: COLORS.white,
+              height: 40,
+              color: COLORS.white,
+              ...FONTS.body3,
+            }}
+            placeholder="Enter Password"
+            placeholderTextColor={COLORS.white}
+            selectionColor={COLORS.white}
+            secureTextEntry={true}
+          />
+          {/* Toggle Password */}
+          <TouchableOpacity
+            style={{
+              position: 'absolute',
+              right: 0,
+              bottom: 10,
+              height: 30,
+              width: 30,
+            }}
+            onPress={() => console.log('toggle')}>
+            <Image
+              source={icons.eye}
+              style={{height: 20, width: 20, tintColor: COLORS.white}}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+
+  function renderButtion() {
+    return (
+      <View style={{margin: SIZES.padding * 3}}>
+        <TouchableOpacity
+          style={{
+            height: 60,
+            backgroundColor: COLORS.black,
+            borderRadius: SIZES.radius / 1.5,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+          onPress={() => console.log('button pressed')}>
+          <Text style={{color: COLORS.white, ...FONTS.h3}}>Continue</Text>
+        </TouchableOpacity>
+      </View>
     );
   }
 
@@ -48,6 +226,15 @@ const SignUp = () => {
         <ScrollView>
           {/* Headers */}
           {renderHeader()}
+
+          {/* Logo */}
+          {renderLogo()}
+
+          {/* Form */}
+          {renderForm()}
+
+          {/* Button */}
+          {renderButtion()}
         </ScrollView>
       </LinearGradient>
     </KeyboardAvoidingView>
